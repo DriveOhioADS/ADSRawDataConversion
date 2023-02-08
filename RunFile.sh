2142b8a55d96a47e8c0a2e28f7e0b3c93d8d167e
Bob <rg383015@ohio.edu>
2023-02-08 10:28:37 -0500

    Upload DataBase to S3

--- RunFile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Starting Program"
-mongod --port 27017 --syslog --fork --dbpath /tmp/ --bind_ip localhost
-python3 IntroScript.py --type cyber --folder cyber --filebase cyber && python3 datainsert.py --config cybersettings.json
-echo "Program Finished"
